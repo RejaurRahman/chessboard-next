3efa40f51b09e02dcd0b0b6599213d2628809adb
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2023-04-10 19:29:19 +0100

    FIX: board cells mapped

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,72 +2,15 @@ import React from "react"
 import styles from "./Board.module.scss"
 
 export default function Board() {
+  const items = []
+
+  for (let i = 0; i < 64; i++) {
+    items.push(<div key={i} className={styles.board__item} />)
+  }
+
   return (
     <div className={styles.board}>
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
-      <div className={styles.board__item} />
+      {items}
     </div>
   )
 }
